e12b472ba0410b6b850a9a5b606282b58735cf67
Leo Park <leopark3846@gmail.com>
2018-01-30 19:03:06 +0000

    split router file api and client.

--- apps/iot/routers/api.router.php
+++ apps/iot/routers/api.router.php
@@ -1,19 +1,4 @@
 <?php
-// default index action, GET /
-$app->get('/', 'Iot\Controller\IndexController:actionIndex')
-    ->name('get-homepage');
-
-$app->group('/v1', function () use ($app) {
-    // get programmers list, GET /v1/programmers
-    $app->get('/programmers', 'Iot\Controller\ProgrammerController:actionGetProgrammers')
-        ->name('get-programmers-list');
-
-    // get programmer detail, GET /v1/programmers/:id
-    $app->get('/programmers/:id', 'Iot\Controller\ProgrammerController:actionGetProgrammer')
-        ->conditions(array('id' => '\d+'))
-        ->name('get-programmer-detail');
-});
-
 $app->group('/api', function () use ($app) {
 
     // USER API
